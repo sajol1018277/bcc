6a3c5b553d814fa8ecee440dbb17131f8b85a339
sajol1018277 <128016052+sajol1018277@users.noreply.github.com>
2023-03-30 11:40:18 +0600

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
+sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
+echo "Remember to logoff/reboot for the changes to take effect."
